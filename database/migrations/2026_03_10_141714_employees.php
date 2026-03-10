c0491a28af05536427b4651f741d8dff51927d1e
Geoff Patrick <geoffgranada20@gmail.com>
2026-03-10 22:31:19 +0800

    Schemas

---
+++ database/migrations/2026_03_10_141714_employees.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id('employee_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table ->string('phone_number');
+            $table ->date('hire_date');
+            $table ->id('job_id');
+            $table->decimal('salary', 8, 2);
+            $table ->int('manager_id')->nullable();
+            $table->int('department_id')->nullable();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
---
+++ database/migrations/2026_03_10_142646_locations.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('locations',function(Blueprint $table){
+            $table->id('location_id');
+            $table->string('street_address');
+            $table->string('postal_code');
+            $table->string('city');
+            $table->string('state_province');
+            $table->id('country_id');
+        }
+        );
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
